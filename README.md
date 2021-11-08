1b7ccac7df564009ff6cfa3c7688bb4fa07e83c8
sherKuo <sherilynkuo@gmail.com>
2021-11-07 23:11:21 -0800

    screenshots

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 7 - Starter Code
+Worked by myself
+https://sherkuo.github.io/Lab7_Starter/
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,38 @@
 
 import { Router } from './Router.js';
 
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+});
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
 const recipes = [
   'https://introweb.tech/assets/json/ghostCookies.json',
   'https://introweb.tech/assets/json/birthdayCake.json',
